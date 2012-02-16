98e656efc199cab18068cf70f267f9963b3bc799
frenchFlozza <flozza@gossap.fr>
2012-02-16 13:56:02 +0100

    Update src/com/massivecraft/factions/cmd/CmdAutoHelp.java

--- src/com/massivecraft/factions/cmd/CmdAutoHelp.java
+++ src/com/massivecraft/factions/cmd/CmdAutoHelp.java
@@ -43,6 +43,6 @@ public class CmdAutoHelp extends MCommand<P>
 			}
 		}
 		
-		sendMessage(p.txt.getPage(lines, this.argAsInt(0, 1), "Help for command \""+pcmd.aliases.get(0)+"\""));
+		sendMessage(p.txt.getPage(lines, this.argAsInt(0, 1), "Aide pour la commande \""+pcmd.aliases.get(0)+"\""));
 	}
 }
